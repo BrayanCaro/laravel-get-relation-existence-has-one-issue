865f9900fef96535043165cf4c5e6030b659e043
Brayan Martínez Santana <brayanmtz@ciencias.unam.mx>
2025-01-19 20:30:07 -0600

    Update seeder with minimal example

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Post;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -13,11 +14,26 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
+        $firstUser = User::factory()->create([
+            'name' => 'Alpha',
+        ]);
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
+        Post::create([
+            'id' => 10,
+            'user_id' => $firstUser->id,
+        ]);
+        Post::create([
+            'id' => 888,
+            'user_id' => $firstUser->id,
+        ]);
+
+        $secondUser = User::factory()->create([
+            'name' => 'Beta',
+        ]);
+
+        Post::create([
+            'id' => 999,
+            'user_id' => $secondUser->id,
         ]);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,23 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    $relationship = User::query()->getModel()->post();
+
+    $existenceQuery = $relationship->getRelationExistenceQuery(
+        $relationship->getRelated()->query(),
+        User::query(),
+        ['id'],
+    );
+
+    $query = User::orderBy($existenceQuery, 'desc');
+
+    // $query->ddRawSql();
+
+    // User Beta should be first
+    // User Alpha should be second
+    // @see database/seeders/DatabaseSeeder.php
+    return $query->get();
 });
